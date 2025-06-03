5f6993b401cff2698f95de9d822be5af892f04cf
João Carneiro <153639757+JoaoCarneiroo@users.noreply.github.com>
2025-06-03 20:46:36 +0100

    adicionado dialectModule

--- Back-End/Database/sequelize.js
+++ Back-End/Database/sequelize.js
@@ -7,15 +7,17 @@ const { Sequelize } = require('sequelize');
 // });
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,       
-  process.env.DB_USER,       
-  process.env.DB_PASSWORD,   
-  {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mariadb',
-    logging: false,          
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mariadb',
+        dialectModule: mariadb,
+
+        logging: false,
+    }
 );
 
 module.exports = sequelize;
