0e8d7f68cf4cca62276ba217d69c4baf846e3781
João Carneiro <153639757+JoaoCarneiroo@users.noreply.github.com>
2025-06-03 20:38:11 +0100

    MariaDB

--- Back-End/Database/sequelize.js
+++ Back-End/Database/sequelize.js
@@ -1,9 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './Database/cafe.db',
-    logging: false, 
-});
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: './Database/cafe.db',
+//     logging: false, 
+// });
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,       
+  process.env.DB_USER,       
+  process.env.DB_PASSWORD,   
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mariadb',
+    logging: false,          
+  }
+);
 
 module.exports = sequelize;
